62c386c8bf91aec1cb2aa0bf4b3854d4282194f9
Curtis Conard <cconard96@gmail.com>
2026-07-08 17:49:35 -0400

    reset versions

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,7 @@
 export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+        'type-enum': [1, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
         'subject-case': [1, 'always', ['sentence-case', 'lower-case']],
         'references-empty': [1, 'never'],
         'header-max-length': [0, 'always']
